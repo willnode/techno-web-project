fdafa0dab62f7128987b122cab060060bb35ace0
Wildan Mubarok <willnode@wellosoft.net>
2018-10-09 16:33:52 +0700

    Uppp

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
 var data = {
 	title: 'Loading...',
 	kategori: [],
-	menu: ["Beranda", "Profil", "Kategori", "Galeri", "Tentang"]
+	lamanAktif: 0,
+	lamanKategori: 0,
+	menu: [{
+		judul: "Beranda",
+		icon: "icon/home.png",
+	}, {
+		judul: "Profil",
+		icon: "icon/profil.png",
+	}, {
+		judul: "Kategori",
+		icon: "icon/kategori.png",
+		submenu: [{
+			judul: "Kerajinan"
+		}, {
+			judul: "Kuliner"
+		}, {
+			judul: "Budaya"
+		}]
+	}, {
+		judul: "Galeri",
+		icon: "icon/galeri.png",
+	}, {
+		judul: "Tentang",
+		icon: "icon/tentang.png",
+	}],
+	pergiKeLaman: function(i) {
+		data.lamanAktif = i;
+		data.lamanKategori = 0;
+	},
+	pergiKeSubLaman: function(i, j) {
+		data.lamanAktif = i;
+		data.lamanKategori = j;
+	}
 }
 
 var app = new Vue({
@@ -10,13 +42,13 @@ var app = new Vue({
 })
 
 var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-	   // Kalo selesai
-	   var obj = JSON.parse(this.responseText);
-	   data.title = obj.judul;
-	   data.kategori = obj.kategori;
-    }
+xhttp.onreadystatechange = function () {
+	if (this.readyState == 4 && this.status == 200) {
+		// Kalo selesai
+		var obj = JSON.parse(this.responseText);
+		data.title = obj.judul;
+		data.kategori = obj.kategori;
+	}
 };
 
 xhttp.open("GET", "server.php", true);
--- server.php
+++ server.php
@@ -1,9 +1,36 @@
 <?php
+
+class Sektor {
+
+	var $judul;
+	var $gambar;
+	var $submenu;
+
+	function __construct($judul, $gambar, $submenu=[])
+	{
+		$this->judul = $judul;
+		$this->gambar = $gambar;
+		$this->submenu = $submenu;
+	}
+}
+
+class Submenu {
+	var $judul;
+	var $deskripsi;
+	var $lokasi;
+
+}
+
+$kerajinan = new Sektor("Kerajinan", "images/kerajinan.jpg");
+$kuliner = new Sektor("Kuliner", "images/kuliner.jpg");
+$budaya = new Sektor("Budaya", "images/budaya.jpg");
+
 $myObj = new stdClass();
 $myObj->judul = "Sistem Informasi Perkonomian Bangkalan";
-$myObj->kategori = array("Kerajinan", "Kuliner", "Budaya");
 
-$myJSON = json_encode($myObj);
+$myObj->kategori = array( $kerajinan, $kuliner, $budaya);
+
+$myJSON = json_encode($myObj, JSON_PRETTY_PRINT);
 
 echo $myJSON;
 ?>
